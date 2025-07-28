0b33ee0c67b9587d54546dfebf5e0bdf5499e35c
NealHelman <nhelman@5thtradition.com>
2025-07-28 05:15:11 -0400

    Enabled theming

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { listenToWalletData } from 'nexus-module';
+import marketplaceTheme from './Styles/theme';
 
 import configureStore from './configureStore';
 import App from './App';
+import './Styles/styles.css';
 
 const store = configureStore();
 listenToWalletData(store);
 
+const {
+  libraries: {
+    React,
+    ReactDOM,
+    emotion: { react, styled, cache },
+  }
+} = NEXUS;
+const { ThemeProvider } = NEXUS.libraries.emotion.react;
+
 const root = createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={marketplaceTheme}>
+      <App />
+    </ThemeProvider>
   </Provider>
 );
